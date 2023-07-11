d86ee0b3530f9bd95c12edab47c5950c84969f41
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-07-12 06:48:31 +0700

    quiz app learning

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 200, 181, 181),
-                Color.fromARGB(255, 246, 32, 32),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(fontSize: 31),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
